7672404e03c9aac20701fdc14e02ef3e474e8ec6
Mohamed Romdhani <m.romdhani@businesstraining.be>
2019-07-18 11:41:08 +0100

    Commit initial du back

---
+++ src/main/java/be/businesstraining/TriponlinesoufianebackApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriponlinesoufianebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriponlinesoufianebackApplication.class, args);
+    }
+
+}
